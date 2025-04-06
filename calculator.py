b0f800672ca42038e39798cd117ac73ec0d79671
n0488629 <n0488629@amarillocollege.com>
2025-04-06 10:03:41 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -8,13 +8,13 @@ print("3 - Multiplication")
 print("4 - Division")
 userinput = int(input("Enter your choice (1-4): ")) # Get user input to decide an operation in the following match case
 match userinput:
-    case 1:
+    case 1: # case for addition
         print("Here is the result: ",number1 + number2)
-    case 2:
+    case 2: # case for subtraction
         print("Here is the result: ",number1 - number2)
-    case 3:
+    case 3: # case for multiplication
         print("Here is the result: ",number1 * number2)
-    case 4:
+    case 4: # case for division
         print("Here is the result: ",number1 / number2)
     case _: #if the user doesnt iunput 1-4
         print("Please enter a number(1-4) to select an operation")
